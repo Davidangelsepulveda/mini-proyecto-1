8d1e41737370e38fbc054a82dd1b453d75149746
David <davidsepulveda583gmail.com>
2025-10-13 16:20:28 -0300

    Agregar configuración de Live Server y funcionalidad de alerta en el formulario de contacto

---
+++ main.js
@@ -0,0 +1,4 @@
+let btnformcontacto = document.getElementById("btnformcontacto");
+btnformcontacto.addEventListener("click", function() {
+    alert("Gracias por contactarnos, en breve nos comunicaremos con usted.");
+});
