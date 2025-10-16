c4a034218d6a400c681b9257184a6e52597416d5
David <davidsepulveda583gmail.com>
2025-10-16 17:29:49 -0300

    Agregar CV en formato PDF y funcionalidad de descarga en el portafolio

--- main.js
+++ main.js
@@ -2,3 +2,19 @@ let btnformcontacto = document.getElementById("btnformcontacto");
 btnformcontacto.addEventListener("click", function() {
     alert("Gracias por contactarnos, en breve nos comunicaremos con usted.");
 });
+document.addEventListener("DOMContentLoaded", () => {
+  const botonCV = document.getElementById("descargarCV");
+  if (botonCV) {
+    botonCV.addEventListener("click", () => {
+      const link = document.createElement("a");
+      link.href = "./cv_final_david_sepulveda_(2).pdf";
+      link.download = "./cv_final_david_sepulveda_(2).pdf";
+      link.target = "_blank";
+
+      document.body.appendChild(link);
+      link.click();
+
+      document.body.removeChild(link);
+    });
+  }
+});
